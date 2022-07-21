ea086ecc23baf06c5549d76d9d14c2a4f7e14e64
Hongju Meow <8851218@hyundai.com>
2022-07-21 09:15:28 +0900

    net's works combined to net class

--- VGGnet.py
+++ VGGnet.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from tqdm import tqdm
+
+from loader import image_loader
 
 class VGG(nn.Module):
     def __init__(self):
@@ -19,3 +22,25 @@ class VGG(nn.Module):
                 features.append(x)
 
         return features
+    
+    def get_features(self, dataset1, dataset2, **args):
+        num_images = args['num_images']
+        
+        feats1 = [0] * 5
+        feats2 = [0] * 5
+        for i in tqdm(range(num_images)):
+            with torch.no_grad():
+                img1_feats = self.forward(image_loader(dataset1[i]).to(args['device'], torch.float))
+                img2_feats = self.forward(image_loader(dataset2[i]).to(args['device'], torch.float))
+
+                if i == 0:
+                    feats1 = [a for a in img1_feats]
+                    feats2 = [a for a in img2_feats]
+                else:
+                    feats1 = [a + b for a, b in zip(feats1, img1_feats)]
+                    feats2 = [a + b for a, b in zip(feats2, img2_feats)]
+        
+        feats1_mean = [a / num_images for a in feats1]
+        feats2_mean = [a / num_images for a in feats2]
+
+        return feats1_mean, feats2_mean
